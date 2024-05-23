412feed9350747ac44cf9494a6bfd090b163b417
Michał <michal.reszka86@gmail.com>
2024-05-23 23:45:26 +0200

    Fix AuthGuard's wrong validate function call

--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -1,29 +1,39 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 import axios from "axios";
 import Main from "./Main"
 
-const validate = async (props) => {
-    const ipAddress = "http://localhost:8080"
-    const token = localStorage.getItem('jwtToken');
-    
-    var auth = false;
-
-    await axios.get(ipAddress + "/api/auth/validate", {
-        headers: { 'Authorization': `Bearer ${token}` }
-    }).then(function(res){
-        console.log(res.data);
-        auth = res.data
-    }).catch(function(err){
-        console.log(err);
-    });
-
-    return auth;
-}
-
-const authStatus = await validate();
 
 const AuthGuard = () => {
+    const token = localStorage.getItem('jwtToken');
+    const [ authStatus, setAuthStatus ] = useState(false);
+    const [ isLoading, setIsLoading ] = useState(true);
+    
+    const validate = async () => {
+        const ipAddress = "http://localhost:8080"
+        
+        try{
+            const res = await axios.get(ipAddress + "/api/auth/validate", {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+
+            console.log("Token verification status: " + res.data);
+            setAuthStatus(res.data);
+            setIsLoading(false);
+        } catch(err){
+            console.log(err);
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        validate();
+    }, [token]);
+
+
+    if (isLoading){
+        return <p>Loading</p>;
+    }
     return authStatus ? <Main/> : <Navigate to="/login"></Navigate>;
 }
 

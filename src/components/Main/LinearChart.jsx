bd4b344835cce8158725ad6ba9871eeedcaec781
Michał <michal.reszka86@gmail.com>
2024-05-26 22:59:21 +0200

    Change the way how dates are presented on chart and improve some looks

--- src/components/Main/LinearChart.jsx
+++ src/components/Main/LinearChart.jsx
@@ -1,54 +1,89 @@
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale
+);
 function LinearChart({ chartData }) {
-    const options = {
-        responsive: true,
-        interaction: {
-            mode: 'index',
-            intersect: false,
+  const options = {
+    responsive: true,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    stacked: false,
+    plugins: {
+      title: {
+        display: true,
+        text: "Zestawienie cen mieszkań oraz stóp procentowych z ostatnich 10 lat",
+        font: {
+          size: 16,
         },
-        stacked: false,
-        plugins: {
-            title: {
-                display: true,
-                text: 'Zestawienie cen mieszkań oraz stóp procentowych z ostatnich 10 lat',
-                font:{
-                    size: 16
-                }
-            },
+      },
+      tooltip: {
+        position: "nearest",
+      },
+    },
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "year",
+          tooltipFormat: "MMM yyyy",
+          displayFormats: {
+            month: "MMM yyyy",
+          },
         },
-        scales: {
-            y: {
-                type: 'linear',
-                display: true,
-                position: 'left',
-                title: {
-                    display: true,
-                    text: 'Średnia cena mieszkania w (zł)',
-                    font: {
-                        size: 16,
-                    }
-                }
-            },
-            y1: {
-                type: 'linear',
-                display: true,
-                position: 'right',
-                grid: {
-                    drawOnChartArea: false,
-                },
-                title: {
-                    display: true,
-                    text: 'Wartość stóp procentowych wyrażona w (%)',
-                    font: {
-                        size: 16
-                    }
-                }
-            },
+      },
+      y: {
+        type: "linear",
+        display: true,
+        position: "left",
+        title: {
+          display: true,
+          text: "Średnia cena mieszkania w (zł)",
+          font: {
+            size: 16,
+          },
         },
-    };
-    return <Line data={chartData} options={options} />;
+      },
+      y1: {
+        type: "linear",
+        display: true,
+        position: "right",
+        grid: {
+          drawOnChartArea: false,
+        },
+        title: {
+          display: true,
+          text: "Wartość stóp procentowych wyrażona w (%)",
+          font: {
+            size: 16,
+          },
+        },
+      },
+    },
+    spanGaps: true,
+  };
+  return <Line data={chartData} options={options} />;
 }
 
 export default LinearChart;
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,100 +1,129 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
 import LinearChart from "./LinearChart";
 import Menu from "./ChartNav";
 import dataService from "../../services/dataService";
-import "./LinearChart.css"
-
+import "./LinearChart.css";
 
 function Main() {
-    const [housingData, setHousingData] = useState([]);
-    const [ratesData, setRatesData] = useState([])
-    const [chartData, setChartData] = useState(null);
-    const [isLoadingHousing, setIsLoadingHousing] = useState(true);
-    const [isLoadingInterests, setIsLoadingInterests] = useState(true);
-    const [region, setRegion] = useState("POLSKA");
-    const [market, setMarket] = useState("rynek pierwotny")
-    const [type, setType] = useState("do 40 m2")
+  const [housingData, setHousingData] = useState([]);
+  const [ratesData, setRatesData] = useState([]);
+  const [chartData, setChartData] = useState(null);
+  const [isLoadingHousing, setIsLoadingHousing] = useState(true);
+  const [isLoadingInterests, setIsLoadingInterests] = useState(true);
+  const [region, setRegion] = useState("POLSKA");
+  const [market, setMarket] = useState("rynek pierwotny");
+  const [type, setType] = useState("do 40 m2");
 
-    useEffect(() => {
-        dataService.getHousingData(region, market, type).then((data) => {
-            setHousingData(data)
-            setIsLoadingHousing(false)
-        })
-    }, [region, market, type]);
+  useEffect(() => {
+    dataService.getHousingData(region, market, type).then((data) => {
+      setHousingData(data);
+      setIsLoadingHousing(false);
+    });
+  }, [region, market, type]);
 
-    useEffect(() => {
-        dataService.getRatesData().then((data) => {
-            setRatesData(data)
-            setIsLoadingInterests(false)
-        })
-    }, [isLoadingInterests])
+  useEffect(() => {
+    dataService.getRatesData().then((data) => {
+      setRatesData(data);
+      setIsLoadingInterests(false);
+    });
+  }, [isLoadingInterests]);
 
-    const mergeDates = (dates1, dates2) => {
-        const uniqueDates = new Set([...dates1, ...dates2]);
-        return Array.from(uniqueDates).sort((a, b) => new Date(a) - new Date(b));
-    };
+  const mergeDates = (dates1, dates2) => {
+    const uniqueDates = new Set([...dates1, ...dates2]);
+    return Array.from(uniqueDates).sort((a, b) => new Date(a) - new Date(b));
+  };
 
-    const mapDataToDates = (dates, dataDates, dataValues, flagFullYears = false) => {
-        const dataMap = new Map(dataDates.map((date, index) => [date, dataValues[index]]));
-        let lastValue = null;
-        return dates.map(date => {
-            if (dataMap.has(date)) {
-                lastValue = dataMap.get(date);
-                return { value: lastValue, isFullYear: flagFullYears };
-            }
-            return { value: lastValue, isFullYear: false };
-        });
-    };
-
-    useEffect(() => {
-        if (!isLoadingHousing && !isLoadingInterests) {
-            const housingDates = housingData.map((d) => `${d.year}-01-01`);
-            const housingPrices = housingData.map((d) => d.price);
-            const interestRateDates = ratesData.map((d) => {
-                const [day, month, year] = d.date.split('-');
-                return `${day}-${month}-${year}`;
-            });
-            const interestRates = ratesData.map((d) => d.refRate);
-
-            const allDates = mergeDates(housingDates, interestRateDates);
-            const mappedHousingPrices = mapDataToDates(allDates, housingDates, housingPrices, true);
-            const mappedInterestRates = mapDataToDates(allDates, interestRateDates, interestRates);
-
-            const parsedData = {
-                labels: allDates,
-                datasets: [
-                    {
-                        label: "Ceny mieszkań",
-                        data: mappedHousingPrices.map((d) => d.value),
-                        borderDash: [5, 5],
-                        yAxisID: "y",
-                        pointRadius: mappedHousingPrices.map((d) => d.isFullYear ? 5 : 0),
-                    },
-                    {
-                        label: "Stopy procentowe",
-                        data: mappedInterestRates.map((d) => d.value),
-                        stepped: true,
-                        yAxisID: 'y1'
-                    }
-                ],
-            }
-            parsedData.labels.pop();
-            parsedData.datasets[0].data.pop();
-            setChartData(parsedData);
-        }
-    }, [isLoadingHousing, isLoadingInterests, housingData, ratesData]);
-
-    return (
-        <div>
-            <Menu region={region} setRegion={setRegion} market={market} setMarket={setMarket} type={type} setType={setType}></Menu>
-            <div className="chartCard">
-                <div className="chartBox">
-                    {chartData ? <LinearChart chartData={chartData} /> : <p></p>}
-                </div>
-            </div>
-        </div>
+  const mapDataToDates = (
+    dates,
+    dataDates,
+    dataValues,
+    flagFullYears = false
+  ) => {
+    const dataMap = new Map(
+      dataDates.map((date, index) => [date, dataValues[index]])
     );
+    let lastValue = null;
+    return dates.map((date) => {
+      if (dataMap.has(date)) {
+        lastValue = dataMap.get(date);
+        return { value: lastValue, isFullYear: flagFullYears };
+      }
+      return { value: lastValue, isFullYear: false };
+    });
+  };
+
+  useEffect(() => {
+    if (!isLoadingHousing && !isLoadingInterests) {
+      const housingDates = housingData.map((d) => `${d.year}-01-01`);
+      const housingPrices = housingData.map((d) => d.price);
+      const interestRateDates = ratesData.map((d) => {
+        const [day, month, year] = d.date.split("-");
+        return `${day}-${month}-${year}`;
+      });
+      const interestRates = ratesData.map((d) => d.refRate);
+
+      const allDates = mergeDates(housingDates, interestRateDates);
+      const mappedHousingPrices = mapDataToDates(
+        allDates,
+        housingDates,
+        housingPrices,
+        true
+      );
+      const mappedInterestRates = mapDataToDates(
+        allDates,
+        interestRateDates,
+        interestRates
+      );
+
+      const parsedData = {
+        labels: allDates,
+        datasets: [
+          {
+            label: "Ceny mieszkań",
+            data: mappedHousingPrices.map((d) =>
+              d.isFullYear ? d.value : null
+            ),
+            borderDash: [5, 5],
+            yAxisID: "y",
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "Stopy procentowe",
+            data: mappedInterestRates.map((d) => d.value),
+            stepped: true,
+            yAxisID: "y1",
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            pointRadius: 0,
+          },
+        ],
+      };
+      parsedData.labels.pop();
+      parsedData.datasets[0].data.pop();
+      setChartData(parsedData);
+    }
+  }, [isLoadingHousing, isLoadingInterests, housingData, ratesData]);
+
+  return (
+    <div>
+      <Menu
+        region={region}
+        setRegion={setRegion}
+        market={market}
+        setMarket={setMarket}
+        type={type}
+        setType={setType}
+      ></Menu>
+      <div className="chartCard">
+        <div className="chartBox">
+          {chartData ? <LinearChart chartData={chartData} /> : <p></p>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;

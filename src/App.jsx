8a953ac6cb57958b8898342c82989b7d746b28e3
Michał <michal.reszka86@gmail.com>
2024-05-27 11:07:21 +0200

    Modify AuthGuard to prevent authorized users from accessing pages for unauthorized ones

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import { useState } from 'react'
-import { Route, Routes, Navigate } from "react-router-dom"
-import Register from "./components/Register"
-import Login from './components/Login'
-import Index from "./components/Index"
-import Main from "./components/Main"
-import AuthGuard from './components/AuthGuard'
+import { Route, Routes, Navigate } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Index from "./components/Index";
+import Main from "./components/Main";
+import AuthGuard from "./components/AuthGuard";
 
 function App() {
-  const token = localStorage.getItem("jwtToken")
-
-  return(
+  return (
     <Routes>
-      
-      {/* {token && <Route path='/' element={<Navigate replace to='/main' />}/>}
-      {token && <Route path='/register' element={<Navigate replace to='/main' />}/>}
-      {token && <Route path='/login' element={<Navigate replace to='/main' />}/>} */}
-      
-      <Route path='/' element={<Index/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/main' element={<AuthGuard/>}/>
+      <Route
+        path="/"
+        element={
+          <AuthGuard onAuth={<Navigate to="/main" />} onNotAuth={<Index />} />
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <AuthGuard
+            onAuth={<Navigate to="/main" />}
+            onNotAuth={<Register />}
+          />
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <AuthGuard onAuth={<Navigate to="/main" />} onNotAuth={<Login />} />
+        }
+      />
+      <Route
+        path="/main"
+        element={
+          <AuthGuard onAuth={<Main />} onNotAuth={<Navigate to="/login" />} />
+        }
+      />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -1,39 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
 import axios from "axios";
-import Main from "./Main";
 
-const AuthGuard = () => {
-	const token = localStorage.getItem("jwtToken");
-	const [authStatus, setAuthStatus] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
+const AuthGuard = (props) => {
+  const token = localStorage.getItem("jwtToken");
+  const [authStatus, setAuthStatus] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-	const validateToken = async () => {
-		try {
-			const res = await axios.get(
-				import.meta.env.VITE_API_SERVER_ADDRESS + "/api/auth/validate",
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
+  const validateToken = async () => {
+    try {
+      const res = await axios.get(
+        import.meta.env.VITE_API_SERVER_ADDRESS + "/api/auth/validate",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
 
-			console.log("Token verification status: " + res.data);
-			setAuthStatus(res.data);
-			setIsLoading(false);
-		} catch (err) {
-			console.log(err);
-			setIsLoading(false);
-		}
-	};
+      console.log("Token verification status: " + res.data);
+      setAuthStatus(res.data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		validateToken();
-	}, [token]);
+  useEffect(() => {
+    validateToken();
+  }, [token]);
 
-	if (isLoading) {
-		return <p>Loading</p>;
-	}
-	return authStatus ? <Main /> : <Navigate to="/login"></Navigate>;
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return authStatus ? props.onAuth : props.onNotAuth;
 };
 
 export default AuthGuard;
